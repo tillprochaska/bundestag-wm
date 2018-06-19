83aa9f431e1823704014814e09b3e023d5af82fb
Till Prochaska <mail@tillprochaska.de>
2018-06-19 17:23:37 +0200

    Fix time zones, use flat list of matches

--- scrapers/matches.py
+++ scrapers/matches.py
@@ -1,40 +1,70 @@
 import requests;
 from bs4 import BeautifulSoup;
 import json;
-from datetime import datetime;
+from datetime import datetime, timezone, timedelta;
 import pytz;
-from dateutil import parser;
 import re;
+import logging;
+logging.basicConfig(level=logging.INFO);
 
-r = requests.get('https://www.fifa.com/worldcup/matches/');
+URL = 'https://www.fifa.com/worldcup/matches/';
+DEST = './data/matches.json';
+
+KNOCKOUT_TYPES = ['round of 16', 'quarter-finals', 'semi-finals', 'play-off for third place', 'final'];
+TIMEZONE_OFFSET = {
+    'Moscow': 3,
+    'Ekaterinburg': 5,
+    'St. Petersburg': 3,
+    'Sochi': 3,
+    'Kazan': 3,
+    'Saransk': 3,
+    'Kaliningrad': 2,
+    'Samara': 4,
+    'Rostov-On-Don': 3,
+    'Nizhny Novgorod': 3,
+    'Volgograd': 3,
+};
+
+def cleanStr(str):
+    str = str.strip();
+    str = re.sub('\s+', ' ', str);
+    return str;
+
+logging.info('Requesting match list…');
+r = requests.get(URL);
 doc = BeautifulSoup(r.text, 'html.parser');
 
 now = datetime.now(pytz.utc);
-groups = [];
+matches = [];
 
 for group in doc.select('.fi-matchlist .fi-mu-list'):
-    name = group.select('.fi-mu-list__head__date')[0].text.strip();
-    name = re.sub('\s+', ' ', name);
-    matches = [];
+    # check if match is a group or knockout match
+    matchType = cleanStr(group.select('.fi-mu-list__head__date')[0].text).lower();
+    if matchType not in KNOCKOUT_TYPES:
+        matchType = 'group'
 
     for match in group.select('.fi-mu'):
-        date = parser.parse(match.select('.fi-mu__info__datetime')[0]['data-utcdate']);
+        city = cleanStr(match.select('.fi__info__venue')[0].text);
+
+        # dates are in the following format: 01 Jul 2018 - 18:00 Local time
+        dateFormat = '%d %b %Y - %H:%M Local time';
+        date = datetime.strptime(cleanStr(match.select('.fi-mu__info__datetime')[0].text), dateFormat);
+        zone = timezone(timedelta(hours = TIMEZONE_OFFSET[city]));
+        date = date.replace(tzinfo = zone);
 
         matches.append({
+            'type': matchType,
             'date': date.isoformat(),
-            'stadium': match.select('.fi__info__stadium')[0].text.strip(),
-            'city': match.select('.fi__info__venue')[0].text.strip(),
+            'stadium': cleanStr(match.select('.fi__info__stadium')[0].text),
+            'city': city,
 
-            'team1': match.select('.fi-t .fi-t__nText')[0].text.strip(),
-            'team2': match.select('.fi-t .fi-t__nText')[1].text.strip(),
+            'team1': cleanStr(match.select('.fi-t .fi-t__nText')[0].text),
+            'team2': cleanStr(match.select('.fi-t .fi-t__nText')[1].text),
 
-            'score': match.select('.fi-s__scoreText')[0].text.strip() if date < now else None,
+            'score': cleanStr(match.select('.fi-s__scoreText')[0].text) if date < now else None,
         });
 
-    groups.append({
-        'name': name,
-        'matches': matches,
-    });
+logging.info('Saved info on %d matches into %s.', len(matches), DEST);
 
-with open('./data/matches.json', 'w+') as file:
-    file.write(json.dumps(groups));
+with open(DEST, 'w+') as file:
+    file.write(json.dumps(matches));
