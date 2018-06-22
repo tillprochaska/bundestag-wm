9c7696c6f48086d1d16ebc687b158c091cf9ce7e
Till Prochaska <mail@tillprochaska.de>
2018-06-22 02:51:53 +0200

    Add country colors and translations

---
+++ app/helpers/countries/index.js
@@ -0,0 +1,140 @@
+const COLORS = {
+	'blue': 'rgb(74, 155, 255)',
+	'lightblue': 'rgb(147, 234, 239)',
+	'black': 'rgb(72, 72, 74)',
+	'green': 'rgb(50, 187, 41)',
+	'red': 'rgb(255, 75, 108)',
+	'orange': 'rgb(255, 201, 3)',
+	'yellow': 'rgb(255, 235, 0)',
+};
+
+export default {
+	'Russia' : {
+		'name': 'Russland',
+		'color': COLORS['blue']
+	},
+	'Germany' : {
+		'name': 'Deutschland',
+		'color': COLORS['']
+	},
+	'Brazil' : {
+		'name': 'Brasilien',
+		'color': COLORS['green']
+	},
+	'Portugal' : {
+		'name': 'Portugal',
+		'color': COLORS['green']
+	},
+	'Argentina' : {
+		'name': 'Argentinien',
+		'color': COLORS['lightblue']
+	},
+	'Belgium' : {
+		'name': 'Belgien',
+		'color': COLORS['black']
+	},
+	'Poland' : {
+		'name': 'Polen',
+		'color': COLORS['red']
+	},
+	'France' : {
+		'name': '',
+		'color': COLORS['blue']
+	},
+	'Spain' : {
+		'name': 'Spanien',
+		'color': COLORS['orange']
+	},
+	'Peru' : {
+		'name': 'Peru',
+		'color': COLORS['red']
+	},
+	'Switzerland' : {
+		'name': 'Schweiz',
+		'color': COLORS['red']
+	},
+	'England' : {
+		'name': 'England',
+		'color': COLORS['red']
+	},
+	'Colombia' : {
+		'name': 'Kolumbien',
+		'color': COLORS['orange']
+	},
+	'Mexico' : {
+		'name': 'Mexiko',
+		'color': COLORS['green']
+	},
+	'Uruguay' : {
+		'name': 'Uruguay',
+		'color': COLORS['blue']
+	},
+	'Croatia' : {
+		'name': 'Kroatien',
+		'color': COLORS['red']
+	},
+	'Denmark' : {
+		'name': 'Dänemark',
+		'color': COLORS['red']
+	},
+	'Iceland' : {
+		'name': 'Island',
+		'color': COLORS['blue']
+	},
+	'Costa Rica' : {
+		'name': 'Costa Rica',
+		'color': COLORS['red']
+	},
+	'Sweden' : {
+		'name': 'Schweden',
+		'color': COLORS['yellow']
+	},
+	'Tunisia' : {
+		'name': 'Tunesien',
+		'color': COLORS['red']
+	},
+	'Egypt' : {
+		'name': 'Ägypten',
+		'color': COLORS['black']
+	},
+	'Senegal' : {
+		'name': 'Senegal',
+		'color': COLORS['green']
+	},
+	'IR Iran' : {
+		'name': 'Iran',
+		'color': COLORS['green']
+	},
+	'Serbia' : {
+		'name': 'Serbien',
+		'color': COLORS['red']
+	},
+	'Nigeria' : {
+		'name': 'Nigerien',
+		'color': COLORS['green']
+	},
+	'Australia' : {
+		'name': 'Australien',
+		'color': COLORS['blau']
+	},
+	'Japan' : {
+		'name': 'Japan',
+		'color': COLORS['red']
+	},
+	'Morocco' : {
+		'name': 'Marokko',
+		'color': COLORS['red']
+	},
+	'Panama' : {
+		'name': 'Panama',
+		'color': COLORS['blau']
+	},
+	'Korea Republic' : {
+		'name': 'S+dkorea',
+		'color': COLORS['Red']
+	},
+	'Saudi Arabia' : {
+		'name': 'Saudi Arabien',
+		'color': COLORS['green']
+	}
+}
